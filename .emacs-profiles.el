3e4739185a09c82e7824f6eef23105cf11866536
Jonathan Prieto-Cubides <jonathan.cubides@uib.no>
2022-04-13 13:18:59 +0200

    Add chemacs as a submodule and doom and spacemacs.

---
+++ .emacs-profiles.el
@@ -0,0 +1,7 @@
+(
+ ("space" . ((user-emacs-directory . "~/dotfiles/spacemacs/.emacs.d")
+        (env . (("SPACEMACSDIR" . "~/dotfiles/spacemacs/.spacemacs.d")))))
+
+ ("doom" . ((user-emacs-directory . "~/dotfiles/doom/.emacs.d")
+        (env . (("DOOMDIR" . "~/dotfiles/doom/.doom.d")))))
+ )
